c400d9ee662afa9cd72d84f770f18a53d2a88edc
mikhail_kurochkin <mkv-1724@mail.ru>
2019-12-07 17:53:51 +0300

    some fix

--- fake_chatbot.py
+++ fake_chatbot.py
@@ -2,4 +2,4 @@ from db import Message
 
 
 def process_question(message):
-    return Message.create(token=message.token, content='Reply to: '+message.content, reply_to=message)
+    return Message.create(token=message.token, content='Reply to: '+message.content, reply_to=message).dicts()
