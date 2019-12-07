57ebd4fb601ae07daecca742ad061fa110ed797b
mikhail_kurochkin <mkv-1724@mail.ru>
2019-12-07 18:00:25 +0300

    some fix 2

--- fake_chatbot.py
+++ fake_chatbot.py
@@ -1,5 +1,6 @@
 from db import Message
+from playhouse.shortcuts import model_to_dict
 
 
 def process_question(message):
-    return Message.create(token=message.token, content='Reply to: '+message.content, reply_to=message).dicts()
+    return model_to_dict(Message.create(token=message.token, content='Reply to: '+message.content, reply_to=message))
